0c2ab81b65719310921191e495cffe1d89d99c3c
James Declerus <james.declerus@esih.edu>
2019-04-10 16:35:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,14 +4,14 @@ class TicTacToe
 def initialize(board = nil)
 @board = board || Array.new(9, " ")
 end
- 
+
 #def current_player
 #turn_count % 2 == 0 ? "X" : "O"
 #end
- 
+
 #def turn_count
 #end
- 
+
 
 
 WIN_COMBINATIONS = [
